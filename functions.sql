1dc1f082fec56d02ae2d1a6d1ac674f8b520d630
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2018-07-22 12:19:05 +0300

    Added new function and made code improvements

--- functions.sql
+++ functions.sql
@@ -1,8 +1,10 @@
-DROP PROCEDURE IF EXISTS `get_first`;
-DROP PROCEDURE IF EXISTS `calculate_age`;
-DROP PROCEDURE IF EXISTS `get_num_of_days`;
-DROP PROCEDURE IF EXISTS `get_num_of_months`;
-DROP PROCEDURE IF EXISTS `get_num_of_years`;
+DROP FUNCTION IF EXISTS `get_first`;
+DROP FUNCTION IF EXISTS `calculate_age`;
+DROP FUNCTION IF EXISTS `get_num_of_days`;
+DROP FUNCTION IF EXISTS `get_num_of_months`;
+DROP FUNCTION IF EXISTS `get_num_of_years`;
+DROP FUNCTION IF EXISTS `get_time_difference`;
+
 
 DELIMITER $$
 
@@ -20,7 +22,7 @@ DELIMITER $$
 
 CREATE FUNCTION `calculate_age`( date_of_birth DATE ) 
 RETURNS TINYINT
-COMMENT 'Returns how old a something or someone is in terms of years'
+COMMENT 'Returns how old something or someone is in terms of years'
 
 BEGIN
     RETURN ABS( TIMESTAMPDIFF(YEAR, date_of_birth, CURRENT_DATE) );
@@ -35,7 +37,7 @@ RETURNS INT
 COMMENT 'Returns the difference in number of days from a given date to another given date'
 
 BEGIN
-    RETURN ABS( TIMESTAMPDIFF(DAY, start_date, end_date) ) + 1;
+    RETURN ABS( TIMESTAMPDIFF(DAY, start_date, end_date) );
 END $$
 
 DELIMITER ;
@@ -47,17 +49,43 @@ RETURNS INT
 COMMENT 'Returns the difference in number of months from a given date to another given date'
 
 BEGIN
-    RETURN ABS( TIMESTAMPDIFF(MONTH, start_date, end_date) ) + 1;
+    RETURN ABS( TIMESTAMPDIFF(MONTH, start_date, end_date) );
 END $$
 
 DELIMITER ;
+
+DELIMITER $$
 
 CREATE FUNCTION `get_num_of_years`( start_date DATE, end_date DATE ) 
 RETURNS INT
 COMMENT 'Returns the difference in number of years from a given date to another given date'
 
 BEGIN
-    RETURN ABS( TIMESTAMPDIFF(YEAR, start_date, end_date) ) + 1;
+    RETURN ABS( TIMESTAMPDIFF(YEAR, start_date, end_date) );
+END $$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE FUNCTION `get_time_difference`( measure CHAR(10), start_date DATE, end_date DATE ) 
+RETURNS INT
+COMMENT 'Returns the difference in number of years or months or days from a given date to another given date'
+
+BEGIN
+    DECLARE difference INT;
+
+    IF measure = 'years' OR measure = 'year' THEN
+        SET difference = ABS( TIMESTAMPDIFF(YEAR, start_date, end_date) );
+    ELSEIF measure = 'months' OR measure = 'month' THEN
+        SET difference = ABS( TIMESTAMPDIFF(MONTH, start_date, end_date) );
+    ELSEIF measure = 'days' OR measure = 'day' THEN
+        SET difference = ABS( TIMESTAMPDIFF(DAY, start_date, end_date) );
+    ELSE 
+        SET difference = ABS( TIMESTAMPDIFF(YEAR, start_date, end_date) );
+    END IF;
+
+    RETURN difference;
 END $$
 
 DELIMITER ;
